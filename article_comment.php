482bf2566426679e172ea31ea40fe99cccdde5de
vattgern <sashawot060@gmail.com>
2023-10-02 17:40:29 +0400

    v.1.4 almost done

--- article_comment.php
+++ article_comment.php
@@ -3,3 +3,27 @@
  * article_id
  * comment_id
  */
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getArticleComments($db){
+    $result = $db->query("SELECT * FROM `article_comment`");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $item){
+        $insert .= "INSERT INTO `article_comment`(`article_id`, `comment_id`) VALUES ('{$item['article_id']}','{$item['comment_id']}'); ";
+    }
+    return $insert;
+}
+
+$db = connectOldDB();
+$articleComments = getArticleComments($db);
+
+$result = madeInsert($articleComments);
+
+$fp = fopen("files/article_comments.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
-//require './users.php';
-
+require './users.php';
 require './articles.php';
+require  './tags.php';
+require './article_tag.php';
+
+require './roles.php';
+require './accesses.php';
+require './accesses_role.php';
+require './user_role.php';
+require './flags.php';
+require  './article_flag.php';
+require './comments.php';
+require './reports.php';
+require './user_sub.php';
+
+require './bookmarks.php';
+require './chapters.php';
+require './article_chapter.php';
+
+require './user_flag.php';
+require './chapter_sub.php';
+require './search_input.php';
+require './media.php';
+require './article_comment.php';
+require './comment_likes.php';
+require './article_dayjest.php';
+require './verifications.php';
