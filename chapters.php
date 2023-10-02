40d7a7df1d708d90a59ef4c949abbdbc6807a68a
vattgern <sashawot060@gmail.com>
2023-10-02 15:41:56 +0400

    v.0.9 article_chapters and chapters done

--- chapters.php
+++ chapters.php
@@ -15,3 +15,29 @@
  * updated_at
  * deleted_at
  */
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getChapters($db){
+    $result = $db->query("SELECT * FROM `chapters`;");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $chapter){
+        $insert .= "INSERT INTO `chapters`(`id`, `title`, `url`, `description`, `avatar_path`, `cover_path`, `is_visible`, `rating`, `score`, ". (empty($chapter['created_at']) ? null : (empty($chapter['updated_at']) ? '`created_at`' : '`created_at`,')) ." ". (empty($chapter['updated_at']) ? null : (empty($chapter['deleted_at']) ? '`updated_at`' : '`updated_at`,')) ." ". (empty($chapter['deleted_at']) ? null : '`deleted_at`') .") VALUES ('".$chapter['id']."','". $chapter['title'] ."','". $chapter['url'] ."','". $chapter['description'] ."','". $chapter['avatar_path'] ."','". $chapter['cover_path'] ."','". $chapter['is_visible'] ."','". $chapter['rating'] ."','". $chapter['score'] ."',". (empty($chapter['created_at']) ? null : (empty($chapter['updated_at']) ? "'". $chapter['created_at'] ."'" : ",'". $chapter['created_at'] ."',")) ."". (empty($chapter['updated_at']) ? null : (empty($chapter['deleted_at']) ? "'". $chapter['updated_at'] ."'" : "'". $chapter['updated_at'] ."',")) ."". (empty($chapter['deleted_at']) ? null : "'". $chapter['deleted_at'] ."'") ."); ";
+    }
+    return $insert;
+}
+
+
+
+
+$db = connectOldDB();
+$chapters = getChapters($db);
+$result = madeInsert($chapters);
+
+$fp = fopen("files/chapters.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
