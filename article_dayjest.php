bcc10a0341ba303b9d9a4eef6b788a005052e4c9
vattgern <sashawot060@gmail.com>
2023-10-02 16:28:14 +0400

    v.1.2 article_dayjest and verifications  done

--- article_dayjest.php
+++ article_dayjest.php
@@ -4,3 +4,27 @@
  * created_at
  * updated_at
  */
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getArticleDayjest($db){
+    $result = $db->query("SELECT * FROM `article_dayjest`;");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $dayjest){
+
+        $insert .= "INSERT INTO `article_dayjest`(`article_id`, `created_at`, `updated_at`) VALUES ('". $dayjest['article_id'] ."','". $dayjest['created_at'] ."','". $dayjest['updated_at'] ."'); ";
+    }
+    return $insert;
+}
+
+$db = connectOldDB();
+$articleDayjest = getArticleDayjest($db);
+$result = madeInsert($articleDayjest);
+
+$fp = fopen("files/article_dayjest.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
--- verifications.php
+++ verifications.php
@@ -9,3 +9,27 @@
  * created_at
  * updated_at
  */
+
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getVerifications($db){
+    $result = $db->query("SELECT * FROM `verifications`;");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $verification){
+        $insert .= "INSERT INTO `verifications`(`id`, `user_id`, ". (empty($verification['pincode']) ? null : '`pincode`,') ." `created_at`, `updated_at`) VALUES ('". $verification['id'] ."','". $verification['user_id'] ."',". (empty($verification['pincode']) ? null : "'". $verification['pincode'] ."',") ." '". $verification['created_at'] ."','". $verification['updated_at'] ."'); ";
+    }
+    return $insert;
+}
+
+$db = connectOldDB();
+$verifications = getVerifications($db);
+$result = madeInsert($verifications);
+
+$fp = fopen("files/verifications.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
