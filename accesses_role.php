bf83fc31d7693b4a5d6a225669c8ec2d58aee368
vattgern <sashawot060@gmail.com>
2023-10-02 13:30:39 +0400

    v.0.6 some foreign keys table done

--- accesses_role.php
+++ accesses_role.php
@@ -5,3 +5,27 @@
  * access_id - access_id
  * role_id - role_id
  */
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getAccessesRole($db){
+    $result = $db->query("SELECT * FROM `access_role`; ");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $accessRole){
+        $insert .= "INSERT INTO `access_role`(`access_id`, `role_id`) VALUES ('" . $accessRole['access_id'] . "','" . $accessRole['role_id'] . "'); ";
+    }
+    return $insert;
+}
+
+
+$db = connectOldDB();
+$roles = getAccessesRole($db);
+$result = madeInsert($roles);
+
+$fp = fopen("files/accesses_role.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
--- user_role.php
+++ user_role.php
@@ -5,3 +5,27 @@
  * user_id - user_id
  * role_id - role_id
  */
+
+function connectOldDB(){
+    return new PDO("mysql:host=localhost;port=3306;dbname=vapenew-old", 'root', '');
+}
+function getUserRole($db){
+    $result = $db->query("SELECT * FROM `user_role`; ");
+    return $result->fetchAll(PDO::FETCH_ASSOC);
+}
+function madeInsert($arr){
+    $insert = '';
+    foreach ($arr as $userRole){
+        $insert .= "INSERT INTO `user_role`(`user_id`, `role_id`) VALUES ('". $userRole['user_id'] ."','". $userRole['role_id'] ."'); ";
+    }
+    return $insert;
+}
+
+
+$db = connectOldDB();
+$roles = getUserRole($db);
+$result = madeInsert($roles);
+
+$fp = fopen("files/user_role.sql", "w");
+fwrite($fp, $result);
+fclose($fp);
